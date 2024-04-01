410dfbd8195f00743cdcf803e15b5e824b2d9381
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-04-01 11:48:11 -0400

    Ahh, false alarm

--- tests/object.test.ts
+++ tests/object.test.ts
@@ -20,7 +20,7 @@ describe("ReactiveObject", () => {
 
     assertReactivelySettled({
       access: () => obj.foo,
-      change: () => (original.foo += "!!"),
+      change: () => (obj.foo += "!!"),
     });
   });
 
