1787db6156096681998d7717131782f8a7c74d74
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-04-01 09:46:31 -0400

    Fix `@signal` decorator types (#2)
    * Are decorator types still bonkers?
    
    * Lint the tests too
    
    * Update workflow

--- src/index.ts
+++ src/index.ts
@@ -4,38 +4,47 @@ import { Signal } from "signal-polyfill";
  * Usage:
  * ```js
  * export class Counter {
- *    @signal accessor #value = 0;
+ *   @signal accessor #value = 0;
  *
- *     get doubled() {
- *       return this.#value * 2;
- *     }
- *
- *     increment() {
- *      this.#value++;
+ *   get doubled() {
+ *     return this.#value * 2;
  *   }
  *
- *     decrement() {
- *      if (this.#value > 0) {
+ *   increment() {
+ *     this.#value++;
+ *   }
+ *
+ *   decrement() {
+ *     if (this.#value > 0) {
  *       this.#value--;
- *    }
+ *     }
+ *   }
  * }
- *  }
  * ```
  */
-export function signal(target: any, _context: any) {
+export function signal<Value = any>(
+  target: ClassAccessorDecoratorTarget<unknown, Value>,
+  _context: ClassAccessorDecoratorContext,
+): ClassAccessorDecoratorResult<unknown, Value> {
   const { get } = target;
 
   return {
-    get() {
-      return get.call(this).get();
+    get(): Value {
+      // SAFETY: does TS not allow us to have a different type internally?
+      //         maybe I did something goofy.
+      return (get.call(this) as Signal.State<Value>).get();
     },
 
-    set(value: unknown) {
-      get.call(this).set(value);
+    set(value: Value) {
+      // SAFETY: does TS not allow us to have a different type internally?
+      //         maybe I did something goofy.
+      (get.call(this) as Signal.State<Value>).set(value);
     },
 
-    init(value: unknown) {
-      return new Signal.State(value);
+    init(value: Value) {
+      // SAFETY: does TS not allow us to have a different type internally?
+      //         maybe I did something goofy.
+      return new Signal.State(value) as unknown as Value;
     },
   };
 }
