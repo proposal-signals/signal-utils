9595a84f94c3f70fdcddd4e936e4b022e4b51f5d
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-04-21 15:27:29 -0400

    Unify @signal. Resolves #43

--- tests/@signal.test.ts
+++ tests/@signal.test.ts
@@ -1,9 +1,9 @@
-import { describe, it } from "vitest";
+import { describe, it, assert } from "vitest";
 
 import { assertStable, assertReactivelySettled } from "./helpers.ts";
 import { signal } from "../src/index.ts";
 
-describe("@signal", () => {
+describe("@signal (accessor)", () => {
   it("works", () => {
     class State {
       @signal accessor #value = 3;
@@ -23,5 +23,43 @@ describe("@signal", () => {
     });
 
     assertStable(() => state.doubled);
+  });
+});
+
+describe("@signal (getter)", () => {
+  it("works", () => {
+    let cachedCalls = 0;
+
+    class State {
+      @signal accessor #value = 3;
+
+      @signal
+      get doubled() {
+        cachedCalls++;
+        return this.#value * 2;
+      }
+
+      increment = () => this.#value++;
+    }
+
+    let state = new State();
+
+    assert.strictEqual(cachedCalls, 0);
+
+    assertReactivelySettled({
+      access: () => state.doubled,
+      change: () => state.increment(),
+    });
+
+    assert.strictEqual(cachedCalls, 2);
+
+    assertStable(() => state.doubled);
+
+    // No more evaluation of the getter
+    assert.strictEqual(cachedCalls, 2);
+    state.doubled;
+    assert.strictEqual(cachedCalls, 2);
+    state.doubled;
+    assert.strictEqual(cachedCalls, 2);
   });
 });
