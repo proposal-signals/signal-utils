246e57f4cee4d6f3b5c2d8c3d0b87c4291b65ca7
Justin Fagnani <justin@fagnani.com>
2024-05-13 13:46:27 -0700

    Add reaction() utility (#61)
    * Add reaction() utility
    
    * Address review comments
    
    * Test the unhandled rejection
    
    ---------
    
    Co-authored-by: NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>

---
+++ src/subtle/reaction.ts
@@ -0,0 +1,75 @@
+import { Signal } from "signal-polyfill";
+
+export const __internal_testing__ = {
+  active: false,
+  lastError: null,
+} as { active: boolean; lastError: null | unknown };
+
+class ReactionError {
+  original: unknown;
+  name = "ReactionError";
+
+  constructor(original: unknown) {
+    this.original = original;
+  }
+}
+
+/**
+ * Reactions are a way to observe a value and run an effect when it changes.
+ *
+ * The `data` function is run and tracked in a computed signal. It returns a
+ * value that is compared to the previous value. If the value changes, the
+ * `effect` function is called with the new value and the previous value.
+ *
+ * @param data A function that returns the value to observe.
+ * @param effect A function that is called when the value changes.
+ * @param equals A function that compares two values for equality.
+ * @returns A function that stops the reaction.
+ */
+export const reaction = <T>(
+  data: () => T,
+  effect: (value: T, previousValue: T) => void,
+  equals = Object.is,
+) => {
+  // Passing equals here doesn't seem to dedupe the effect calls.
+  const computed: Signal.Computed<T> | undefined = new Signal.Computed(data, {
+    equals,
+  });
+  let previousValue = computed.get();
+  let notify: (() => Promise<void>) | undefined = async () => {
+    // await 0 is a cheap way to queue a microtask
+    await 0;
+    // Check if this reaction was unsubscribed
+    if (notify === undefined) {
+      return;
+    }
+    const value = computed.get();
+    if (!equals(value, previousValue)) {
+      try {
+        effect(value, previousValue);
+      } catch (e) {
+        // TODO: we actually want this to be unhandled, but Vitest complains.
+        // We probably don't want to enable dangerouslyIgnoreUnhandledErrors
+        // for all tests
+        if (__internal_testing__) {
+          console.error(e);
+          __internal_testing__.lastError = e;
+        } else {
+          throw new ReactionError(e);
+        }
+      } finally {
+        previousValue = value;
+      }
+    }
+    watcher.watch();
+  };
+  const watcher = new Signal.subtle.Watcher(() => notify?.());
+  watcher.watch(computed);
+  return () => {
+    watcher.unwatch();
+    // TODO: Do we need this? Add a memory leak test.
+    // By severing the reference to the notify function, we allow the garbage
+    // collector to clean up the resources used by the watcher.
+    notify = undefined;
+  };
+};
---
+++ tests/subtle/reaction.test.ts
@@ -0,0 +1,161 @@
+import { describe, test, assert, afterEach } from "vitest";
+import { Signal } from "signal-polyfill";
+import { reaction, __internal_testing__ } from "../../src/subtle/reaction.ts";
+
+afterEach(() => {
+  __internal_testing__.active = false;
+});
+
+describe("reaction()", () => {
+  test("calls the effect function when the data function return value changes", async () => {
+    const count = new Signal.State(0);
+
+    let callCount = 0;
+    let value, previousValue;
+
+    reaction(
+      () => count.get(),
+      (_value, _previousValue) => {
+        callCount++;
+        value = _value;
+        previousValue = _previousValue;
+      },
+    );
+
+    // Effect callbacks are not called immediately
+    assert.strictEqual(callCount, 0);
+
+    await 0;
+
+    // Effect callbacks are not called until the data function changes
+    assert.strictEqual(callCount, 0);
+
+    // Effect callbacks are called when the data function changes
+    count.set(count.get() + 1);
+    await 0;
+    assert.strictEqual(callCount, 1);
+    assert.strictEqual(value, 1);
+    assert.strictEqual(previousValue, 0);
+
+    // is good enough to not freeze / OOM
+    for (let i = 0; i < 25; i++) {
+      count.set(count.get() + 1);
+      await 0;
+      assert.strictEqual(callCount, 2 + i);
+      assert.strictEqual(value, i + 2);
+      assert.strictEqual(previousValue, i + 1);
+    }
+  });
+
+  test("Unsubscribed reactions aren't called", async () => {
+    const count = new Signal.State(0);
+
+    let callCount = 0;
+    const unsubscribe = reaction(
+      () => count.get(),
+      () => {
+        callCount++;
+      },
+    );
+
+    // Check reaction is live
+    count.set(count.get() + 1);
+    await 0;
+    assert.strictEqual(callCount, 1);
+
+    unsubscribe();
+
+    // Check reaction is not live
+    count.set(count.get() + 1);
+    await 0;
+    assert.strictEqual(callCount, 1);
+  });
+
+  test("You can unsubscribe while an effect is pending", async () => {
+    const count = new Signal.State(0);
+
+    let callCount = 0;
+    const unsubscribe = reaction(
+      () => count.get(),
+      () => {
+        callCount++;
+      },
+    );
+
+    // Check reaction is live
+    count.set(count.get() + 1);
+    await 0;
+    assert.strictEqual(callCount, 1);
+
+    // Check reaction is not live
+    count.set(count.get() + 1);
+    unsubscribe();
+    await 0;
+    assert.strictEqual(callCount, 1);
+  });
+
+  test("equal data values don't trigger effect", async () => {
+    const a = new Signal.State(0);
+    const b = new Signal.State(0);
+
+    let callCount = 0;
+
+    reaction(
+      () => a.get() + b.get(),
+      (_value, _previousValue) => {
+        callCount++;
+      },
+    );
+
+    // 1 + -1 still equals 0
+    a.set(1);
+    b.set(-1);
+    await 0;
+    assert.strictEqual(callCount, 0);
+  });
+
+  test("throwing in effect doesn't hang reaction", async () => {
+    const x = new Signal.State(0);
+
+    let callCount = 0;
+    let value, previousValue;
+    let thrown = false;
+
+    if (typeof process !== "undefined") {
+      process.on("uncaughtException", (error) => {
+        console.log("uncaughtException", error);
+      });
+    }
+
+    reaction(
+      () => x.get(),
+      (_value, _previousValue) => {
+        callCount++;
+        value = _value;
+        previousValue = _previousValue;
+        if (value === 1) {
+          thrown = true;
+          throw new Error("Oops");
+        }
+        thrown = false;
+      },
+    );
+
+    __internal_testing__.active = true;
+    x.set(1);
+    await 0;
+    assert.strictEqual(callCount, 1);
+    assert.strictEqual(thrown, true);
+    assert.strictEqual(
+      (__internal_testing__.lastError as Error).message,
+      "Oops",
+    );
+
+    x.set(2);
+    await 0;
+    assert.strictEqual(callCount, 2);
+    assert.strictEqual(thrown, false);
+    assert.strictEqual(value, 2);
+    assert.strictEqual(previousValue, 1);
+  });
+});
