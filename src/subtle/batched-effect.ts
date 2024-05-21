5d1242d10a33894695b50a7c401da778fd50028e
Justin Fagnani <justin@fagnani.com>
2024-05-20 23:22:45 -0700

    Handle exceptions and nested batches

--- src/subtle/batched-effect.ts
+++ src/subtle/batched-effect.ts
@@ -5,28 +5,69 @@ const notifiedEffects = new Set<{
   watcher: Signal.subtle.Watcher;
 }>();
 
+let batchDepth = 0;
+
 /**
- * Runs a function inside a batch, and calls all the effected batched effects
- * synchronously.
+ * Runs the given function inside of a "batch" and calls any batched effects
+ * (those created with `batchEffect()`) that depend on updated signals
+ * synchronously after the function completes.
+ *
+ * Batches can be nested, and effects will only be called once at the end of the
+ * outermost batch.
+ *
+ * Batching does not change how the signal graph updates, or change any other
+ * watcher or effect system. Accessing signals that are updated within a batch
+ * will return their updates value. Other computations, watcher, and effects
+ * created outside of a batch that depend on updated signals will be run as
+ * usual.
  */
 export const batch = (fn: () => void) => {
-  // Run the function to notifiy the sync watchers
-  fn();
+  batchDepth++;
+  try {
+    // Run the function to notifiy watchers
+    fn();
+  } finally {
+    batchDepth--;
 
-  // Copy then clear the notified effects
-  const effects = [...notifiedEffects];
-  notifiedEffects.clear();
+    if (batchDepth !== 0) {
+      return;
+    }
 
-  // Run all the sync callbacks and re-enable the watchers
-  effects.forEach(({ computed, watcher }) => {
-    watcher.watch(computed);
-    computed.get();
-  });
+    // Copy then clear the notified effects
+    const effects = [...notifiedEffects];
+    notifiedEffects.clear();
+
+    // Run all the batched effect callbacks and re-enable the watchers
+    let exceptions!: any[];
+
+    for (const { computed, watcher } of effects) {
+      watcher.watch(computed);
+      try {
+        computed.get();
+      } catch (e) {
+        (exceptions ??= []).push(e);
+      }
+    }
+
+    if (exceptions !== undefined) {
+      if (exceptions.length === 1) {
+        throw exceptions![0];
+      } else {
+        throw new AggregateError(
+          exceptions!,
+          "Multiple exceptions thrown in batched effects",
+        );
+      }
+    }
+  }
 };
 
 /**
- * Creates an effect that runs synchronously inside a batch, when changes are
- * contained within a batch() call.
+ * Creates an effect that runs synchronously at the end of a `batch()` call if
+ * any of the signals it depends on have been updated.
+ *
+ * The effect also runs asynchronously, on the microtask queue, if any of the
+ * signals it depends on have been updated outside of a `batch()` call.
  */
 export const batchedEffect = (effectFn: () => void) => {
   const computed = new Signal.Computed(effectFn);
