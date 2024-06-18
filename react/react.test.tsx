1dc18ed2e1803cee455357f8453c777b62a6412d
David Sheldrick <d.j.sheldrick@gmail.com>
2024-06-18 15:28:12 +0300

    simplify useSignalValue

--- react/react.test.tsx
+++ react/react.test.tsx
@@ -105,8 +105,10 @@ describe("useSignalState", () => {
 describe("useSignalValue", () => {
   it("allows subscribing to signals individually outside of a tracked component", async () => {
     const signal = new Signal.State(0);
+    let setState;
     const App = () => {
       const value = useSignalValue(signal);
+      const [state, _setState] = React.useState(value);
       return <div>count: {value}</div>;
     };
     render(<App />);
@@ -122,5 +124,47 @@ describe("useSignalValue", () => {
     });
 
     expect(screen.getByText("count: 2")).toBeInTheDocument();
+  });
+
+  it("allows using normal hooks too", async () => {
+    const signal = new Signal.State(0);
+    let setState;
+    const App = () => {
+      const value = useSignalValue(signal);
+      const [state, _setState] = React.useState("a");
+      setState = _setState;
+      return (
+        <div>
+          count: {state}
+          {value}
+        </div>
+      );
+    };
+    render(<App />);
+    expect(screen.getByText("count: a0")).toBeInTheDocument();
+
+    await act(() => {
+      signal.set(1);
+    });
+    expect(screen.getByText("count: a1")).toBeInTheDocument();
+
+    await act(() => {
+      signal.set(2);
+    });
+
+    expect(screen.getByText("count: a2")).toBeInTheDocument();
+
+    await act(() => {
+      setState("b");
+    });
+
+    expect(screen.getByText("count: b2")).toBeInTheDocument();
+
+    await act(() => {
+      setState("c");
+      signal.set(3);
+    });
+
+    expect(screen.getByText("count: c3")).toBeInTheDocument();
   });
 });
--- react/useSignalValue.ts
+++ react/useSignalValue.ts
@@ -17,59 +17,43 @@ export function useSignalValue<Value>(fn: () => Value, deps: unknown[]): Value;
 /** @public */
 export function useSignalValue() {
   const args = arguments;
-  // deps will be either the computed or the deps array
   const deps = args.length === 2 ? args[1] : [args[0]];
 
-  const isInRender = useRef(true);
-  isInRender.current = true;
-
   const $val = useMemo(() => {
-    if (args.length === 1) {
-      return args[0];
-    }
     return new Signal.Computed(() => {
-      if (isInRender.current) {
-        return args[0]();
-      } else {
-        try {
-          return args[0]();
-        } catch {
-          // when getSnapshot is called outside of the render phase &
-          // subsequently throws an error, it might be because we're
-          // in a zombie-child state. in that case, we suppress the
-          // error and instead return a new dummy value to trigger a
-          // react re-render. if we were in a zombie child, react will
-          // unmount us instead of re-rendering so the error is
-          // irrelevant. if we're not in a zombie-child, react will
-          // call `getSnapshot` again in the render phase, and the
-          // error will be thrown as expected.
-          return {};
-        }
+      if (args.length === 1) {
+        return args[0].get();
       }
+      return args[0]();
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
 
-  try {
-    const { subscribe, getSnapshot } = useMemo(() => {
-      let listen = () => {};
-      const watcher = new Signal.subtle.Watcher(() => listen());
-      return {
-        subscribe: (_listen: () => void) => {
-          listen = _listen;
-          watcher.watch($val);
-          return () => watcher.unwatch($val);
-        },
-        getSnapshot: () => {
-          watcher.watch();
-          return $val.get();
-        },
-      };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [$val]);
+  const { subscribe, watcher, getSnapshot } = useMemo(() => {
+    let listen = () => {};
+    let numSchedules = 0;
+    const watcher = new Signal.subtle.Watcher(async () => {
+      await 0;
+      if ($val.isPending()) {
+        numSchedules++;
+        listen();
+      }
+    });
+    return {
+      subscribe: (_listen: () => void) => {
+        listen = _listen;
+        watcher.watch($val);
+        return () => watcher.unwatch($val);
+      },
+      watcher,
+      getSnapshot: () => {
+        return numSchedules;
+      },
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [$val]);
 
-    return useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
-  } finally {
-    isInRender.current = false;
-  }
+  watcher.watch();
+  useSyncExternalStore(subscribe, getSnapshot, getSnapshot);
+  return $val.get();
 }
